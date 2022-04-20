2093cfff1fe67e4781cf14257da32ba48fd7a125
dullmatian <someone_6262@yahoo.co.jp>
2022-04-20 15:49:38 +0900

    file name changed

--- renderer/__init__.py
+++ renderer/__init__.py
@@ -1,6 +1,6 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 loader = Environment(
-    loader=PackageLoader('templater', 'templates'),
+    loader=PackageLoader('renderer', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
